f4c2793927d898282ac1ea113f58f43822f276ca
reinaldo-sre <reinaldo.solano@gmail.com>
2025-06-06 16:01:44 -0300

    add module trainer-api

--- trainer-api/src/main/java/com/rsscloud/GreetingResource.java
+++ trainer-api/src/main/java/com/rsscloud/GreetingResource.java
@@ -6,11 +6,11 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class TrainerResource {
+public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST com Lula";
+        return "Hello from Quarkus REST";
     }
 }
--- trainer-api/src/test/java/com/rsscloud/GreetingResourceIT.java
+++ trainer-api/src/test/java/com/rsscloud/GreetingResourceIT.java
@@ -3,6 +3,6 @@ package com.rsscloud;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class TrainerResourceIT extends TrainerResourceTest {
+class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
 }
