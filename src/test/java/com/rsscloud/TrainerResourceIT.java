8d334a8dd451bbbd40b11729544c433a8472eb3a
reinaldo-sre <reinaldo.solano@gmail.com>
2025-06-06 15:14:30 -0300

    primeiro commit

---
+++ src/test/java/com/rsscloud/TrainerResourceIT.java
@@ -0,0 +1,8 @@
+package com.rsscloud;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class TrainerResourceIT extends TrainerResourceTest {
+    // Execute the same tests but in packaged mode.
+}
